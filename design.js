0fdda6c22831ed39d5621f3f9390b52e71891496
lakshmidusanapudi <85738180+lakshmidusanapudi@users.noreply.github.com>
2023-07-28 16:02:29 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,16 @@
+ReactDOM.render(
+    <div>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStZ3A58LWsI8PK61wdwzFVFYp3jwykod9eg5Fdb1_dgQxE0AiFBksWOYrft54POaYlevE&usqp=CAU"/>
+    <h1>Arunima Sinha</h1>
+    <h3>Early Life and Childhood</h3>
+    <p>She was born on 20th of July 1988 in the state of Uttar Pradesh in Ambedkar Nagar. When she was three years old her father died and she could never feel the love of her father. It was from very early age that she was quite passionate about sports and it was that passion that led her to become a national level Volleyball player. But, a dramatic change in her life took place when one of her legs had to cut off because of a train accident.</p>
+    <h3>The Dramatic Change in her Life (Train Accident)</h3>
+    <p>In the year 2011, April, while she was travelling from Lucknow to Delhi from the Padmavati Express, she had a grotesque encounter with a bunch of some robbers. While she was protesting to save her chain and other valuables, there occurred a scuffle, and she was pushed out from a running train. And, she fell on an adjacent train and another train crushed one of her legs which had to be later amputated.</p>
+    <p>For whole of the night she kept lying on the track for whole of the night and many trains ran through her feet and unfortunately she was not able to do anything to save herself. It was only in the morning that villagers noticed and took her to a nearby hospital where her leg below knees was amputated. The sports ministry of India also offered her a compensation of Rs 25000 and she was provided with the best treatment possible.</p>
+    <p>When asked about the incident she said that, "I resisted and they pushed me out of the train. I could not move. I remember seeing a train coming towards me. I tried getting up. By then, the train had run over my leg. I don't remember anything after that."</p>
+    <p>After a national outrage, she was given an additional medical compensation of Rs 200,000 from the Minister of State for Youth Affairs and Sports Ajay Maken. And, in addition to that she was also given a job recommendation in CISF and was offered a job in Indian Railways. She was admitted to the All India Institute of Medical Sciences and where she spent four months. A Delhi based private company also offered her a prosthetic leg free of cost.</p>
+    <h3>Career</h3>
+    <p>There were some negative information with regards to her accident and that made her determination to do something great even more. She decided in her mind to become a mountaineer apart from being Volleyball player. People and doctors would wonder listening to her plans and many started believing that she is going through some mental trauma. But, that was actually her infallible determination to do something unusually great in life.</p>
+    <p>At that point in time she was extremely inspired by the Indian cricketer Yuvraj Singh. After getting released from the hospital she went to Banchendri Pal straight away instead of going home. Bachendri Pal was the first woman who climbed Mount Everest who inspired Arunima to work dedicatedly towards her dreams.</p>
+    </div>,
+    document.getElementById("content"));
